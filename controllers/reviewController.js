dacdebab2b7087879187eaf3a7143a567fe670ac
Muhammad Shehzad <Muhammad.Shehzad@sar.local>
2022-12-22 10:43:46 +0530

    users can only review a tour that they have actually booked

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,6 +1,8 @@
 const Review = require(`${__dirname}/../models/reviewModel`);
-const catchAsync = require(`${__dirname}/../utils/catchAsync`);
+const Booking = require(`${__dirname}/../models/bookingModel`);
 const factory = require('./handlerFactory');
+const catchAsync = require(`${__dirname}/../utils/catchAsync`);
+const AppError = require(`${__dirname}/../utils/appError`);
 
 exports.setTourUserIds = (req, res, next) => {
     // Allow nested routes
@@ -8,6 +10,16 @@ exports.setTourUserIds = (req, res, next) => {
     if(!req.body.user) req.body.user = req.user.id;
     next();
 };
+
+// verify user before review if he actually booked a tour
+exports.userCheckBooking = catchAsync(async (req, res, next) => {
+    const bookings = await Booking.findOne({ user: req.body.user, tour: req.body.tour });
+    if(!bookings){
+        return next(new AppError('Only Users that booked Tour Give Review', 404));
+    }
+
+    next();
+});
 
 // Create Review
 exports.createReview = factory.createOne(Review);
